14c13bae207a93185b6ca9cf83e8ebd9824a5113
Chris Jagernath <jagernat@sheridancollege.ca>
2023-04-01 05:47:47 -0400

    added multiple guess functionality

--- src/components/Guesses.tsx
+++ src/components/Guesses.tsx
@@ -13,18 +13,39 @@ export const Guesses: React.FC<GuessesProps> = ({
   winnerName,
   onSubmitGuess,
 }) => {
+  const [guessResult, setGuessResult] = useState<boolean>();
+  const [guesses, setGuesses] = useState<string[]>([""]);
+
   const handleGuessSubmit = (guessedCorrect: boolean) => {
     //if guess is wrong render another guess element
     onSubmitGuess(guessedCorrect);
+    setGuessResult(guessedCorrect);
+
+    if (!guessedCorrect && guesses.length < 6) {
+      setGuesses([...guesses, "guess"]);
+    }
   };
 
   return (
     <div>
-      <NewGuess winnerName={winnerName} onSubmit={handleGuessSubmit} />
-      {guessNum != 5 ? (
-        <p>You have {6 - guessNum} guesses left</p>
+      {guessResult ? (
+        <div>
+          You got it<button>Share {guessNum}</button>
+        </div>
       ) : (
-        <p>You have 1 guess left</p>
+        <div>
+          {guesses.map((i) => (
+            <div>
+              <NewGuess winnerName={winnerName} onSubmit={handleGuessSubmit} />
+            </div>
+          ))}
+
+          {guessNum != 5 ? (
+            <p>You have {6 - guessNum} guesses left</p>
+          ) : (
+            <p>You have 1 guess left{winnerName}</p>
+          )}
+        </div>
       )}
     </div>
   );
