f1a75798de3df609fcc74e8eee3c4f906e9d6b97
Chris Jagernath <jagernat@sheridancollege.ca>
2023-04-04 21:32:41 -0400

    added dmca info modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,6 +35,33 @@ export const Header: React.FC<HeaderProps> = ({ dailyCount, onClick }) => {
         <h1 className="text-5xl font-bold">Weebdle #{dailyCount}</h1>
       </div>
       <div className="navbar-end">
+        <label htmlFor="DMCA" className="btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              d="M3 9C3 7.89543 3.89543 7 5 7H6.5C7.12951 7 7.72229 6.70361 8.1 6.2L9.15 4.8C9.52771 4.29639 10.1205 4 10.75 4H13.25C13.8795 4 14.4723 4.29639 14.85 4.8L15.9 6.2C16.2777 6.70361 16.8705 7 17.5 7H19C20.1046 7 21 7.89543 21 9V18C21 19.1046 20.1046 20 19 20H5C3.89543 20 3 19.1046 3 18V9Z"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <circle
+              cx="12"
+              cy="13"
+              r="4"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </label>
+
         <label htmlFor="stats" className="btn">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -70,10 +97,53 @@ export const Header: React.FC<HeaderProps> = ({ dailyCount, onClick }) => {
         </label>
       </div>
       {/* Put this part before </body> tag */}
+      <input type="checkbox" id="DMCA" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h1 className="text-3xl font-bold">DMCA Request</h1>
+          <br />
+          <div>
+            To request images to no longer be supported on the site, please send
+            a request through email to weebdle@gmail.com with the following
+            info:
+          </div>
+          <br />
+          <div className="text-left">Subject: DMCA Infringement Notice</div>
+          <br />
+          <div className="text-left">
+            Name of IP Holder requesting the removal.
+          </div>
+          <br />
+          <div className="text-left">
+            A legal certification that you represent the IP Holder in this
+            matter.
+          </div>
+          <br />
+          <div className="text-left">Address and contact information.</div>
+          <br />
+          <div className="text-left">Name of IP URLs of Images to remove</div>
+          <br />
+          <div className="text-left">
+            Name of IP #2 URLs of Images to remove
+          </div>
+          <br />
+          <div className="text-left">
+            Missing information means we will be unable to handle your request.
+            We should be able to get to it within 24-72 hours.
+          </div>
+
+          <div className="modal-action">
+            <label htmlFor="DMCA" className="btn">
+              Close
+            </label>
+          </div>
+        </div>
+      </div>
+
       <input type="checkbox" id="stats" className="modal-toggle" />
       <div className="modal">
         <div className="modal-box">
-          <h1>Statistics</h1>
+          <h1 className="text-3xl font-bold">Statistics</h1>
           <div>1: 1</div>
           <div>2: 3</div>
           <div>3: 4</div>
