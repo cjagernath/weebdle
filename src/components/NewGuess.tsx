6ed4638e07ce9a167467c97408600cd95c941929
Chris Jagernath <jagernat@sheridancollege.ca>
2023-04-05 02:11:34 -0400

    ui

--- src/components/NewGuess.tsx
+++ src/components/NewGuess.tsx
@@ -41,8 +41,21 @@ export const NewGuess: React.FC<NewGuessProps> = ({
         ))}
       </datalist>
       {disableGuess ? (
-        <button className="btn btn-accent btn-sm" disabled={true}>
-          X
+        <button className="btn btn-square btn-disabled btn-sm">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
         </button>
       ) : (
         <button
