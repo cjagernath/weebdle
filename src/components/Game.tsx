9b7fea58a108656700bb6e2adf916600dbaf95f9
Chris Jagernath <jagernat@sheridancollege.ca>
2023-04-13 00:44:03 -0400

    reverted changes and added to array list

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -9,7 +9,11 @@ interface GameProps {
   today: Date;
 }
 
-export const Game: React.FC<GameProps> = ({ onFinished, dailyCount }) => {
+export const Game: React.FC<GameProps> = ({
+  onFinished,
+  dailyCount,
+  today,
+}) => {
   const [guessNum, setGuessNum] = useState(1);
   const [winner, setWinner] = useState(-1);
   const [winnerName, setWinnerName] = useState("");
@@ -18,34 +22,36 @@ export const Game: React.FC<GameProps> = ({ onFinished, dailyCount }) => {
   const [animeNamesList, setAnimeNamesList] = useState<string[]>([]);
   const guessesStats = [0, 0, 0, 0, 0, 0, 0, 0];
   const [savedGuessesArray, setSavedGuessesArray] = useState<number[]>([]);
-  const now = new Date();
-  const timezoneOffset = now.getTimezoneOffset() * 60 * 1000; // convert minutes to milliseconds
-  const today = new Date(now.getTime() - timezoneOffset);
   const dayOfYear = Math.ceil(
     (today.getTime() - new Date(today.getFullYear(), 0, 1).getTime()) /
       (1000 * 60 * 60 * 24)
   );
   const currentDay = today.toDateString();
   const maxGuesses = [1, 2, 3, 4, 5, 6];
-  console.log(dayOfYear);
-  console.log(today);
   function GetAnimeByDate(dayOfYear: number) {
     const animeNames: string[] = [];
     Animes.map((anime) => animeNames.push(anime.name));
     setAnimeNamesList(animeNames);
 
     const animesByDay = [
-      7, 18, 11, 16, 3, 3, 3, 6, 7, 5, 3, 6, 1, 4, 18, 2, 7, 11, 13, 0, 1, 8,
-      11, 6, 1, 0, 17, 6, 10, 6, 7, 8, 14, 6, 4, 4, 8, 6, 7, 2, 10, 6, 16, 4,
-      12, 6, 7, 8, 17, 6, 9, 12, 12, 16, 14, 2, 16, 17, 11, 12, 12, 1, 7, 8, 10,
-      7, 16, 12, 12, 3, 17, 12, 16, 7, 2, 10, 3, 14, 3, 3, 3, 10, 9, 0, 8, 0,
-      16, 12, 11, 11, 1, 16, 2, 9, 6, 0, 4, 16, 6, 0, 8, 0, 6, 9, 5, 7, 6, 11,
-      4, 19, 5, 7, 14, 4, 4, 4, 2, 17, 14, 7, 5, 4, 7, 4, 11, 0, 4, 7, 6, 2, 17,
-      0, 3, 5, 2, 5, 3, 5, 7, 5, 1, 12, 4, 19, 6, 10, 8, 19, 12, 10, 2, 10, 3,
-      14, 16, 18, 6, 10, 2, 0, 6, 17, 8, 10, 0, 6, 18, 8, 2, 1, 0, 1, 16, 6, 17,
-      7, 0, 1, 9, 9, 19, 1, 2, 17, 19, 3, 5, 2, 5, 3, 3, 7, 5, 18, 5, 16, 4, 11,
-      4, 7, 18, 17, 10, 14, 2, 1, 14, 2, 2, 8, 5, 3, 5, 10, 5, 10, 19, 7, 4, 19,
-      9, 19, 12, 0, 5, 7, 11, 17, 16, 11, 6, 6, 16, 14, 1, 6, 17, 18,
+      10, 5, 5, 3, 8, 1, 19, 15, 16, 13, 8, 14, 14, 11, 9, 10, 9, 10, 19, 8, 4,
+      1, 1, 13, 7, 11, 17, 19, 5, 19, 5, 15, 9, 8, 13, 12, 11, 2, 16, 1, 2, 19,
+      4, 10, 2, 10, 6, 11, 2, 6, 1, 1, 5, 7, 3, 19, 9, 15, 15, 11, 10, 6, 5, 11,
+      17, 8, 3, 17, 18, 2, 16, 11, 5, 2, 10, 10, 6, 6, 14, 18, 12, 15, 18, 0,
+      14, 5, 0, 14, 5, 0, 17, 15, 12, 3, 16, 12, 11, 9, 4, 13, 3, 8, 10, 14, 17,
+      14, 19, 16, 9, 10, 13, 2, 8, 11, 11, 15, 13, 10, 19, 3, 8, 12, 1, 19, 16,
+      10, 19, 13, 9, 3, 13, 2, 5, 7, 9, 9, 0, 2, 7, 2, 0, 4, 16, 7, 9, 8, 8, 2,
+      0, 6, 4, 2, 6, 2, 15, 11, 14, 15, 16, 16, 17, 0, 15, 13, 2, 2, 1, 12, 5,
+      4, 11, 19, 3, 15, 9, 14, 15, 14, 14, 5, 2, 3, 0, 2, 0, 9, 11, 14, 15, 4,
+      14, 10, 9, 7, 8, 15, 12, 3, 16, 6, 14, 0, 15, 11, 15, 14, 16, 7, 4, 0, 3,
+      10, 9, 1, 5, 7, 11, 13, 17, 10, 4, 8, 10, 8, 1, 14, 3, 9, 9, 3, 5, 3, 10,
+      16, 6, 16, 4, 5, 11, 13, 16, 10, 9, 0, 4, 15, 9, 8, 5, 8, 13, 0, 13, 11,
+      17, 9, 11, 11, 3, 17, 10, 7, 5, 8, 12, 2, 11, 5, 0, 2, 3, 6, 18, 8, 8, 7,
+      17, 11, 13, 4, 15, 0, 9, 6, 2, 15, 14, 5, 17, 6, 0, 8, 9, 14, 18, 7, 2,
+      15, 5, 17, 18, 1, 9, 0, 13, 11, 1, 13, 12, 16, 6, 18, 3, 5, 2, 13, 8, 4,
+      1, 5, 19, 9, 1, 11, 8, 17, 3, 7, 4, 0, 10, 19, 9, 5, 9, 11, 3, 10, 9, 19,
+      2, 17, 12, 8, 5, 4, 17, 17, 0, 7, 18, 2, 11, 10, 10, 13, 14, 9, 5, 6, 15,
+      3, 2, 9, 7,
     ];
     const winner = animesByDay[dayOfYear];
     const winnerName = animeNames[winner];
